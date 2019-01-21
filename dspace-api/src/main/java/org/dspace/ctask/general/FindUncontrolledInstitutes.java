e907b3f27e5f0620f1578284aba0a52e223563b6
Oliver Goldschmidt <o.goldschmidt@tuhh.de>
2019-01-21 13:35:59 +0100

    improves detection of institutes without authority entry

---
+++ dspace-api/src/main/java/org/dspace/ctask/general/FindUncontrolledInstitutes.java
@@ -0,0 +1,130 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.ctask.general;
+
+import org.apache.log4j.Logger;
+import org.dspace.content.Metadatum;
+import org.dspace.content.MetadataValue;
+import org.dspace.content.Bitstream;
+import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.core.Constants;
+import org.dspace.core.Context;
+import org.dspace.curate.AbstractCurationTask;
+import org.dspace.curate.Curator;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.dspace.authorize.AuthorizeException;
+import java.sql.SQLException;
+
+
+/**
+ * A basic link checker that is designed to be extended. By default this link checker
+ * will check that all links stored in anyschema.anyelement.uri metadata fields return
+ * a 20x status code.
+ *
+ * This link checker can be enhanced by extending this class, and overriding the
+ * getURLs and checkURL methods.
+ *
+ * @author Oliver Goldschmidt
+ */
+
+public class FindUncontrolledInstitutes extends AbstractCurationTask
+{
+
+    // The status of the link checking of this item
+    private int status = Curator.CURATE_UNSET;
+
+    // The results of link checking this item
+    private List<String> results = null;
+
+    // The results that we'll return
+    private StringBuilder resultsString = new StringBuilder();
+
+    private static final String NEW_ITEM_HANDLE = "in workflow";
+
+    // The log4j logger for this class
+    private static Logger log = Logger.getLogger(TypeSetter.class);
+
+
+    /**
+     * Perform the link checking.
+     *
+     * @param dso The DSpaaceObject to be checked
+     * @return The curation task status of the checking
+     * @throws java.io.IOException THrown if something went wrong
+     */
+    @Override
+    public int perform(DSpaceObject dso) throws IOException
+    {
+        //MetadataValue.PARENT_PLACEHOLDER_VALUE
+
+
+        // Unless this is  an item, we'll skip this item
+        status = Curator.CURATE_SKIP;
+        if (dso.getType() == Constants.ITEM)
+        {
+            Item item = (Item)dso;
+            if (!getItemHandle(item).equals(NEW_ITEM_HANDLE)) {
+            try {
+                Context context = Curator.curationContext();
+                Metadatum[] institutes = item.getMetadata("tuhh", "publication", "institute", Item.ANY);
+                if (institutes.length == 0) {
+                    resultsString.append("No institute set for ").append(getItemHandle(item)).append("<br/>!\n");
+                }
+                else {
+                    for (Metadatum institute : institutes) {
+                        String auth = institute.authority;
+                        if (auth != null) {
+                            resultsString.append("No authority for item ").append(getItemHandle(item)).append("<br/>!\n");
+                        }
+                    }
+                }
+
+                status = Curator.CURATE_SUCCESS;
+            } catch (SQLException sqle) {
+                // Something went wrong
+                logDebugMessage(sqle.getMessage());
+                status = Curator.CURATE_ERROR;
+            }
+            }
+        }
+
+        setResult(resultsString.toString());
+        report(resultsString.toString());
+
+        return status;
+    }
+
+    /**
+     * Debugging logging if required
+     *
+     * @param message The message to log
+     */
+    private void logDebugMessage(String message)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug(message);
+        }
+    }
+
+    private static String getItemHandle(Item item)
+    {
+        String handle = item.getHandle();
+        return (handle != null) ? handle: NEW_ITEM_HANDLE;
+    }
+
+}
