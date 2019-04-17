d32a63ff7e34defcee9e430e0e98a58447a381fa
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2019-04-17 18:30:01 +0200

    [CST-390] Improve listener to unindex CRISObjects from Solr dedup core

--- dspace-cris/api/src/main/java/org/dspace/app/cris/deduplication/CrisDedupListenerSolrIndexer.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/deduplication/CrisDedupListenerSolrIndexer.java
@@ -12,10 +12,12 @@ import org.dspace.app.cris.deduplication.service.DedupService;
 import org.dspace.app.cris.model.ACrisObject;
 import org.dspace.core.Context;
 
+import it.cilea.osd.common.listener.NativePostDeleteEventListener;
 import it.cilea.osd.common.listener.NativePostUpdateEventListener;
 import it.cilea.osd.common.model.Identifiable;
 
-public class CrisDedupListenerSolrIndexer implements NativePostUpdateEventListener
+public class CrisDedupListenerSolrIndexer implements NativePostUpdateEventListener,
+    NativePostDeleteEventListener
 {
 
     private static Logger log = Logger
@@ -55,6 +57,39 @@ public class CrisDedupListenerSolrIndexer implements NativePostUpdateEventListen
         catch (Exception ex)
         {
             log.error("Failed to update CRIS metadata in deduplication index for cris-"
+                    + crisObject.getPublicPath() + " uuid:" + crisObject.getUuid());
+        }
+        finally
+        {
+            if (ctx != null && ctx.isValid())
+            {
+                ctx.abort();
+            }
+        }
+    }
+
+    @Override
+    public <P> void onPostDelete(P entity)
+    {
+        Object object = entity;
+        if (!(object instanceof ACrisObject))
+        {
+            // nothing to do
+            return;
+        }
+
+        log.debug("Call onPostUpdate " + CrisDedupListenerSolrIndexer.class);
+
+        ACrisObject crisObject = (ACrisObject)object;
+        Context ctx = null;
+        try
+        {
+            ctx = new Context();
+            dedupService.unIndexContent(ctx, crisObject);
+        }
+        catch (Exception ex)
+        {
+            log.error("Failed to delete CRIS metadata in deduplication index for cris-"
                     + crisObject.getPublicPath() + " uuid:" + crisObject.getUuid());
         }
         finally
