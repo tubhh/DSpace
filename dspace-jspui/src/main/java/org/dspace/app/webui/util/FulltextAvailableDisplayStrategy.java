dc49a8ac83ad114d951f24a493fba9a1216f7012
Oliver Goldschmidt <o.goldschmidt@tuhh.de>
2019-05-16 16:16:10 +0200

    refs 1459, refs RHD-2499: handle virtual metadata in fulltext display strategy correctly

--- dspace-jspui/src/main/java/org/dspace/app/webui/util/FulltextAvailableDisplayStrategy.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/util/FulltextAvailableDisplayStrategy.java
@@ -7,6 +7,8 @@
  */
 package org.dspace.app.webui.util;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
@@ -14,6 +16,8 @@ import javax.servlet.jsp.PageContext;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.dspace.content.DCDate;
+import org.dspace.core.Context;
+import org.dspace.content.Item;
 import org.dspace.content.Metadatum;
 import org.dspace.core.I18nUtil;
 
@@ -24,9 +28,32 @@ public class FulltextAvailableDisplayStrategy extends ASimpleDisplayStrategy
     
     public String getMetadataDisplay(HttpServletRequest hrq, int limit,
             boolean viewFull, String browseType, int colIdx, int itemid, String field,
-            Metadatum[] metadataArray, boolean disableCrossLinks, boolean emph)
+            Metadatum[] metadataArray, boolean disableCrossLinks, boolean emph) throws JspException
     {
         String metadata = I18nUtil.getMessage("jsp.mydspace.render.nofulltext", hrq.getLocale());
+        if (metadataArray.length == 0) {
+            try {
+                // Virtual metadata is not in the metadataArray, so we have to obtain it newly
+                Context obtainContext = UIUtil.obtainContext(hrq);
+                Item item = Item.find(obtainContext, itemid);
+                String[] parts = field.split("\\.");
+                String schema = Item.ANY;
+                String element = Item.ANY;
+                String qualifier = Item.ANY;
+                if (parts.length > 0) {
+                    schema = parts[0];
+                }
+                if (parts.length > 1) {
+                    element = parts[1];
+                }
+                if (parts.length > 2) {
+                    qualifier = parts[2];
+                }
+                metadataArray = item.getMetadata(schema, element, qualifier, Item.ANY);
+            } catch (SQLException e) {
+                throw new JspException(e);
+            }
+        }
         if (metadataArray.length > 0)
         {
             String value = metadataArray[0].value;
